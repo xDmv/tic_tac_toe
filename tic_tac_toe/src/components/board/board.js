02636b0d357fbe9984b9cc50201489cd6839abd1
Daemon <makushin.dima@gmail.com>
2019-11-14 08:19:41 +0200

    fix game

--- tic_tac_toe/src/components/board/board.js
+++ tic_tac_toe/src/components/board/board.js
@@ -2,11 +2,43 @@ import React, { Component } from 'react';
 import Square from '../square/square';
 
 export default class Board extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			squares: Array(9).fill(null),
+			xIsNext: true,
+		};
+	}
+
+	handleClick(i){
+		const squares = this.state.squares.slice();
+		if(this.calculateWinner(squares) || squares[i]){
+			return;
+		}
+		squares[i] = this.state.xIsNext ? 'X': 'O';
+		this.setState({
+			squares: squares,
+			xIsNext: !this.state.xIsNext,
+		});
+	}
+
 	renderSquare(i) {
-		return <Square />;
+		return (
+			<Square 
+				value={this.state.squares[i]}
+				onClick={() => this.handleClick(i)}
+			/>
+		);
 	}
 	render() {
-		const status = 'Next player: X';
+		//const status = 'Следующий ход: ' + (this.state.xIsNext ? 'X': 'O');
+		const winner = this.calculateWinner(this.state.squares);
+		let status;
+		if (winner) {
+			status = 'Выиграл ' + winner;
+		} else {
+			status = 'Следующий ход: ' + (this.state.xIsNext ? 'X': 'O');
+		}
 
 		return (
 			<div>
@@ -29,4 +61,24 @@ export default class Board extends Component {
 			</div>
 			);
 		}
+	
+	calculateWinner(squares) {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i];
+			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				return squares[a];
+			}
+		}
+		return null;
 	}
+}
--- tic_tac_toe/src/components/square/square.js
+++ tic_tac_toe/src/components/square/square.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 
 export default class Square extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: null,
-		};
-	}
-	
+
 	render() {
 		return (
 			<button
 				className="square"
-				onClick={() => this.setState({value: 'X'})}
+				onClick={this.props.onClick}
 			>
-				{this.state.value}
+				{this.props.value}
 			</button>
 		);
 	}
