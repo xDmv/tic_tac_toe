0cb70d0e6975b1c0e0d079cf205e136802831e8a
Daemon <makushin.dima@gmail.com>
2019-11-24 12:31:57 +0200

    up: del code in the Board component

--- tic_tac_toe/src/components/board/board.js
+++ tic_tac_toe/src/components/board/board.js
@@ -2,47 +2,18 @@ import React, { Component } from 'react';
 import Square from '../square/square';
 
 export default class Board extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			squares: Array(9).fill(null),
-			xIsNext: true,
-		};
-	}
-
-	handleClick(i){
-		const squares = this.state.squares.slice();
-		if(this.calculateWinner(squares) || squares[i]){
-			return;
-		}
-		squares[i] = this.state.xIsNext ? 'X': 'O';
-		this.setState({
-			squares: squares,
-			xIsNext: !this.state.xIsNext,
-		});
-	}
 
 	renderSquare(i) {
 		return (
 			<Square 
-				value={this.state.squares[i]}
-				onClick={() => this.handleClick(i)}
+				value={this.props.squares[i]}
+				onClick={() => this.props.onClick(i)}
 			/>
 		);
 	}
 	render() {
-		//const status = 'Следующий ход: ' + (this.state.xIsNext ? 'X': 'O');
-		const winner = this.calculateWinner(this.state.squares);
-		let status;
-		if (winner) {
-			status = 'Выиграл ' + winner;
-		} else {
-			status = 'Следующий ход: ' + (this.state.xIsNext ? 'X': 'O');
-		}
-
 		return (
 			<div>
-				<div className="status">{status}</div>
 				<div className="board-row">
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
@@ -61,24 +32,5 @@ export default class Board extends Component {
 			</div>
 			);
 		}
-	
-	calculateWinner(squares) {
-		const lines = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8],
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8],
-			[0, 4, 8],
-			[2, 4, 6],
-		];
-		for (let i = 0; i < lines.length; i++) {
-			const [a, b, c] = lines[i];
-			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-				return squares[a];
-			}
-		}
-		return null;
-	}
+
 }
